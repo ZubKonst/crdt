1e6eccf33fae914d7fc0cbb8d160f4584c9bef55
Konstantin Zub <hello@zubkonst.com>
2016-04-30 21:24:28 -0400

    implement LWWSet#exists? and LWWSet#get

--- app/lww_set.rb
+++ app/lww_set.rb
@@ -8,22 +8,49 @@ class LWWSet
     @remove_set = MonotonicHash.new
   end
 
-  # hsh.add(element, new_time)  -> lww_set
+  # lww_ser.add(element, new_time)  -> lww_set
   #
-  # Add element into MonotonicHash @add_set.
+  # Adds element and timestamp into MonotonicHash @add_set.
   #
   def add(element, new_time)
     @add_set[element] = new_time
     self
   end
 
-  # hsh.remove(element, new_time)  -> lww_set
+  # lww_set.remove(element, new_time)  -> lww_set
   #
-  # Add element into MonotonicHash @remove_set.
+  # Adds element and timestamp into MonotonicHash @remove_set.
   #
   def remove(element, new_time)
     @remove_set[element] = new_time
     self
+  end
+
+  # lww_set.exists?(element)  -> true or false
+  #
+  # Returns true if the timestamp of the entry from @add_set is more recent than that of the entry from @remove_set.
+  # Returns false if the element isn’t present, or the entry from @remove_set is newer than that from @add_set.
+  #
+  def exists?(element)
+    add_time    = @add_set[element]
+    return false unless add_time
+    remove_time = @remove_set[element]
+    return true unless remove_time
+    add_time > remove_time
+  end
+
+  # lww_set.get  -> an_array
+  #
+  # Returns a new array populated with `existing` elements from this set.
+  # `existing` elements are those elements that are present in @add_set without also being present in @remove_set,
+  # or where the timestamp for the element in @add_set is newer than the timestamp for the element in @remove_set.
+  #
+  def get
+    remove_times = @remove_set.to_h
+    @add_set.to_a.select do |element, add_time|
+      remove_time = remove_times[element]
+      !remove_time || remove_time < add_time
+    end.map(&:first)
   end
 
 end
--- test/test_lww_set.rb
+++ test/test_lww_set.rb
@@ -1,11 +1,105 @@
 require_relative 'test_helper'
 
 class TestLWWSet < Minitest::Test
+
   def setup
     @lww_set = LWWSet.new
+    @add_set    = @lww_set.instance_variable_get(:@add_set)
+    @remove_set = @lww_set.instance_variable_get(:@remove_set)
   end
 
-  def test_that_set_is_truthy
-    assert @lww_set
+  def test_that_add_uses_add_set
+    mock_sets!
+    @add_set.expect :[]=, true, [:test_key, 42]
+    @lww_set.add(:test_key, 42)
+    @add_set.verify
   end
+
+  def test_that_remove_uses_remove_set
+    mock_sets!
+    @remove_set.expect :[]=, true, [:test_key, 42]
+    @lww_set.remove(:test_key, 42)
+    @remove_set.verify
+  end
+
+  def test_entries_when_no_entries
+    stub_sets!(nil, nil)
+    assert !@lww_set.exists?(:test_key)
+    assert_equal [], @lww_set.get
+  end
+
+  def test_entries_when_entry_added
+    stub_sets!(rand(1000), nil)
+    assert @lww_set.exists?(:test_key)
+    assert_equal [:test_key], @lww_set.get
+  end
+
+  def test_entries_when_entry_just_removed
+    stub_sets!(nil, rand(1000))
+    assert !@lww_set.exists?(:test_key)
+    assert_equal [], @lww_set.get
+  end
+
+  def test_entries_when_entry_added_and_removed
+    stub_sets!(1, 2)
+    assert !@lww_set.exists?(:test_key)
+    assert_equal [], @lww_set.get
+  end
+
+  def test_entries_when_entry_removed_before_adding
+    stub_sets!(2, 1)
+    assert @lww_set.exists?(:test_key)
+    assert_equal [:test_key], @lww_set.get
+  end
+
+  def test_sets_with_n_keys
+    add_set = {}
+    remove_set = {}
+    result_keys = []
+
+    100.times.each do |i|
+      case rand
+        when 0...0.25
+          # just added
+          result_keys << "key_#{i}"
+          add_set["key_#{i}"] = rand(1000)
+        when 0.25...0.5
+          # just removed
+          remove_set["key_#{i}"] = rand(1000)
+        when 0.5...0.75
+          # added and removed
+          time = rand(1000)
+          add_set["key_#{i}"] = time
+          remove_set["key_#{i}"] = time + rand(1000)
+        when 0.75...1
+          # removed and added
+          result_keys << "key_#{i}"
+          time = rand(1000)
+          add_set["key_#{i}"] = time + rand(1000)
+          remove_set["key_#{i}"] = time
+      end
+    end
+    @lww_set.instance_variable_set(:@add_set, add_set)
+    @lww_set.instance_variable_set(:@remove_set, remove_set)
+
+    assert_equal result_keys.sort, @lww_set.get.sort
+  end
+
+  private
+
+  def mock_sets!
+    @add_set = Minitest::Mock.new
+    @remove_set = Minitest::Mock.new
+    @lww_set.instance_variable_set(:@add_set, @add_set)
+    @lww_set.instance_variable_set(:@remove_set, @remove_set)
+  end
+
+  def stub_sets!(add_set_value, remove_set_value)
+    add_set = add_set_value ? {test_key: add_set_value} : {}
+    @lww_set.instance_variable_set(:@add_set, add_set)
+
+    remove_set = remove_set_value ? {test_key: remove_set_value} : {}
+    @lww_set.instance_variable_set(:@remove_set, remove_set)
+  end
+
 end
